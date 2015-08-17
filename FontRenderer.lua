971860055c0d7e96fbb72f3a70a4b3936bcc9c98
E. Brown <thecrazyp3nguin@gmail.com>
2015-08-16 20:59:48 -0400

    Updated FontRenderer to use new data format
    Now uses the data from the custom tool.

--- FontRenderer.lua
+++ FontRenderer.lua
@@ -3,8 +3,8 @@ FontRenderer.__index = FontRenderer
 FontRenderer.ClassName = "FontRenderer"
 
 local function GetCharData(fontData, char)
-	for _, charEntry in ipairs(fontData.chars) do
-		if charEntry.char == char then
+	for _, charEntry in ipairs(fontData.Characters) do
+		if charEntry.Char == char then
 			return charEntry
 		end
 	end
@@ -22,13 +22,12 @@ function FontRenderer:RenderCharacter(char)
 	
 	local charData = GetCharData(self.FontData, char)
 	assert(charData, "no char data for "..char.." in font")
-	for i,v in pairs(charData) do print(i,v) end
 	local label = Instance.new("ImageLabel")
 	label.BackgroundTransparency = 1
-	label.Size = UDim2.new(0, charData.w, 0, self.FontData.texture.height)
-	label.ImageRectSize = Vector2.new(charData.w, self.FontData.texture.height)
-	label.Image = self.FontData.file
-	label.ImageRectOffset = Vector2.new(charData.x, 0)
+	label.Size = UDim2.new(0, charData.ImageWidth, 0, charData.ImageHeight)
+	label.ImageRectSize = Vector2.new(charData.ImageWidth, charData.ImageHeight)
+	label.Image = self.FontData.Image
+	label.ImageRectOffset = Vector2.new(charData.ImageX, charData.ImageY)
 	
 	return label
 end
@@ -39,12 +38,14 @@ function FontRenderer:RenderString(str)
 	container.BackgroundTransparency = 1
 	
 	for char in str:gmatch(".") do
+		
 		local charData = GetCharData(self.FontData, char)
+		print(char, charData.Char)
 		local label = self:RenderCharacter(char)
-		label.Position = UDim2.new(0, x + math.floor(charData.ox / 2 + 0.5), 0, 0)
+		label.Position = UDim2.new(0, x, 1, -charData.ImageHeight)
 		label.Parent = container
 		label.Name = char
-		x = x + charData.width
+		x = x + charData.AdvanceWidth
 	end
 	
 	return container
